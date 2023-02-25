c48c9400cdde1a36e690cb5ea12e969abc8a131f
abdussalam-mustapha <abdussalammustapha07@gmail.com>
2023-02-25 02:12:22 -0800

    new update

--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -6,6 +6,17 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <section className="navbar_section">
+        <nav className="navbar">
+            <div className="logo_section">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <p className="logo_name"></p>
+            </div>
+            <div className="nav_btn">
+                <button className="btn">Download App</button>
+            </div>
+        </nav>
+    </section>
 </body>
 </html>
